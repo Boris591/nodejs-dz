b5127fdf33c3e1c576a80d522bce660722c5e0af
boriskrivolapov <boris.kr07@gmail.com>
2024-01-10 23:02:30 +0300

    finish task

--- 5-multithreads/index.js
+++ 5-multithreads/index.js
@@ -1,10 +1,21 @@
 import os from 'os';
 import {Worker} from "node:worker_threads";
+import { performance, PerformanceObserver } from "node:perf_hooks";
 import arrayGenerator from "./modules/arrayGenerator/arrayGenerator.js";
 import getPartsArray from "./modules/getPartsArray/getPartsArray.js";
+import divThree from "./modules/divThree/divThree.js";
 
 const cpuCores = os.cpus().length;
-let count = 0;
+let countForWorker = 0;
+let countForSimple = 0;
+const arr = arrayGenerator(1, 300000);
+
+const performanceObserver = new PerformanceObserver((items) => {
+    items.getEntries().forEach((entry) => {
+        console.log(`${entry.name}: ${entry.duration}`);
+    });
+});
+performanceObserver.observe({ entryTypes: ['measure'] });
 
 const compute = (array) => {
     return new Promise((resolve, reject) => {
@@ -12,20 +23,29 @@ const compute = (array) => {
             workerData: {array}
         });
         worker.on('message', (msg) => {
-            count += msg;
+            countForWorker += msg;
             resolve(msg);
-        });
-        worker.on('error', (msg) => {
-            reject(msg);
         });
     });
 }
 
-const main = async () => {
-    const arr = arrayGenerator(1, 300000);
+const mainWorker = async () => {
     const arrParts = getPartsArray(arr, cpuCores);
+    performance.mark('worker start');
     const calc = arrParts.map(el => compute(el));
-    Promise.all(calc).then(() => console.log(count));
+    Promise.all(calc).then(() => {
+        performance.mark('worker end');
+        performance.measure('worker', 'worker start', 'worker end');
+        console.log('worker res: ', countForWorker)
+    });
 }
 
-main();
+const mainSimple = () => {
+    performance.mark('simple start');
+    countForSimple = divThree({array: arr});
+    performance.mark('simple end');
+    performance.measure('simple', 'simple start', 'simple end');
+    console.log('simple res: ', countForSimple);
+}
+mainSimple();
+mainWorker();
