2ca5f8a0b27bf71f31effc5ede749cd1c9479332
boriskrivolapov <boris.kr07@gmail.com>
2024-01-23 01:16:53 +0300

    notify message

--- 7-timer-notifications/index.js
+++ 7-timer-notifications/index.js
@@ -1,15 +1,16 @@
 import getTime from "./get-time.js";
-import {NotifySend} from "node-notifier";
+import NotifySend from "node-notifier";
 
 
 const time = process.argv[2];
 
 if(time){
     const ms = getTime(time);
-    const notifier =  new NotifySend();
     console.log(`Таймер запущен на ${time}...` );
-    setTimeout(() => notifier.notify({
-        title: 'Таймер сработал!'
+    setTimeout(() => NotifySend.notify({
+        title: 'Таймер сработал!',
+        message: 'Время вышло =)',
+        sound:  true,
     }), ms);
 }else{
     console.warn("Введите время!");
